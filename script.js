0178247a96b7ed30878cc6a2aff609ba0c6bfedc
Minh Pham <giaminh2042004@gmail.com>
2025-02-21 09:55:30 +0700

    feat: add functionality to mark todos as done and delete todos with modal updates

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+fetchTodos();
+
 let todos = [];
 let currentPage = 1;
 const todosPerPage = 10;
@@ -42,4 +44,50 @@ document.getElementById("prev-page").addEventListener("click", () => {
     currentPage--;
     renderTodos();
   }
+});
+
+async function markAsDone(todoId) {
+  const todo = todos.find((t) => t.id === todoId);
+  todo.completed = !todo.completed;
+  await fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`, {
+    method: "PUT",
+    body: JSON.stringify(todo),
+    headers: { "Content-Type": "application/json" },
+  });
+  renderTodos();
+}
+
+async function deleteTodo(todoId) {
+  await fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`, {
+    method: "DELETE",
+  });
+  todos = todos.filter((t) => t.id !== todoId);
+  renderTodos();
+}
+
+let updatingTodoId = null;
+
+function showUpdateModal(todoId) {
+  updatingTodoId = todoId;
+  const modal = document.getElementById("update-modal");
+  modal.style.display = "block";
+  const todo = todos.find((t) => t.id === todoId);
+  document.getElementById("update-title").value = todo.title;
+}
+
+document.getElementById("cancel-update").addEventListener("click", () => {
+  document.getElementById("update-modal").style.display = "none";
+});
+
+document.getElementById("save-update").addEventListener("click", async () => {
+  const newTitle = document.getElementById("update-title").value;
+  const todo = todos.find((t) => t.id === updatingTodoId);
+  todo.title = newTitle;
+  await fetch(`https://jsonplaceholder.typicode.com/todos/${updatingTodoId}`, {
+    method: "PUT",
+    body: JSON.stringify(todo),
+    headers: { "Content-Type": "application/json" },
+  });
+  document.getElementById("update-modal").style.display = "none";
+  renderTodos();
 });
